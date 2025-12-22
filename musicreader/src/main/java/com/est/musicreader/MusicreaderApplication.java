514fd8b5cefcd82d0a7c5979de9ece325bbef8b0
EST-06 <Esteguz06@gmail.com>
2025-12-22 17:30:17 -0500

    Cambios

--- musicreader/src/main/java/com/est/musicreader/MusicreaderApplication.java
+++ musicreader/src/main/java/com/est/musicreader/MusicreaderApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicreaderApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MusicreaderApplication.class, args);
-		System.out.println("Mamon");
+		SpringApplication.run(MusicreaderApplication.class, args);		
 	}
 
 }
